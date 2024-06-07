2ce6ea048ad76922aa61cce8db164491b5c27987
amitap018 <amitap018@gmail.com>
2024-06-07 17:19:20 +0530

    First Commit

---
+++ src/main/java/com/example/CcmsApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CcmsApplication.class, args);
+	}
+
+}
